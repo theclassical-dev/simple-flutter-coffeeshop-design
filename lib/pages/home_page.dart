1d8adbc89f51bafdde06c107ac2960a1fa332316
Abraham Peter <47416051+theclassical-dev@users.noreply.github.com>
2022-08-27 11:43:29 +0100

    bottomBarNavidationbar added  to the homepage

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: Icon(Icons.menu),
+        actions: const [
+          Padding(
+              padding: EdgeInsets.only(right: 20.0),
+            child: Icon(Icons.person)
+            ,
+          ),
+        ],
+        
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.notifications), label: ''),
+      ],),
+      body: Column(
+
+        children: const [
+          //find
+          Text("Find the best coffee for you"),
+
+          //search
+
+
+          //horizontal
+        ],
+      ),
+
+    );
+  }
+}
